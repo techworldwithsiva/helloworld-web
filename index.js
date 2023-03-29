069ab79978f139dd70853818d64cc54d1849313a
siva <siva@ivs.tv>
2023-03-29 12:11:46 +0530

    msg

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Hello, World!');
+});
+
+const port = 3000;
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
